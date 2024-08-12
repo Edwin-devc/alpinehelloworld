4719383d0543bc29a18b078f7d469f03e1ec9532
Rwakasiisi Edwin <erwakasiisi@gmail.com>
2024-08-12 15:22:00 +0300

    Testing the pipeline

--- webapp/app.py
+++ webapp/app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello world!'
+    return 'Hello world! Here to see if the changes sent to github are picked up'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
